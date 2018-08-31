c61192e7db64b2eeec9b33a4c8ae3e738f0c698e
meganutexas <megan.avery720@gmail.com>
2018-08-30 21:24:59 -0500

    update to ConstructionEstimateExpansion.java

--- 08.5 Review Construction Estimate/ConstructionEstimateExpansion.java
+++ 08.5 Review Construction Estimate/ConstructionEstimateExpansion.java
@@ -21,26 +21,24 @@ public class ConstructionEstimateExpansion {
         return areaOfFloor * 7.5;
     }
 
-    public static double fenceLength(double length,
-                                     double width) {
-        return 2 * width + length;
+    public static double calculateFenceLength(double length,
+                                              double width) {
+        return length + (2 * width);
     }
 
-    public static double estimateBackyardFence(double length,
-                                               double width) {
-        return fenceLength(length, width) * 12.5;
+    public static double estimateBackyardFenceCost(double fenceLength) {
+        return fenceLength * 12.5;
+    }
+
+    public static double estimateFrontYardFenceCost(double fenceLength) {
+        return (fenceLength * 6.25) + 50;
     }
 
     public static double estimateBathroomCost(double length,
                                               double width) {
-        int numberExtraBathrooms = (int) (calculateArea(length, width) / 1000);
-        return (1 + numberExtraBathrooms) * 800;
-    }
-
-    public static double estimateFrontYardFenceCost(double length,
-                                                    double width) {
-        double fenceCost = fenceLength(length, width) * 6.25;
-        return fenceCost + 50;
+        double area = calculateArea(length, width);
+        int numberOfBathrooms = 1 + ((int) (area / 1000));
+        return numberOfBathrooms * 800;
     }
 
     public static void printEstimateOfHouse(int length,
@@ -50,16 +48,21 @@ public class ConstructionEstimateExpansion {
 
         double costOfRoof = estimateRoofCost(lengthOfHouse, widthOfHouse);
         double costOfFloor = estimateFloorCost(lengthOfHouse, widthOfHouse);
-        double costOfBackyardFence = estimateBackyardFence(lengthOfHouse, widthOfHouse);
+
+        double widthOfFence = widthOfHouse / 2;
+        double fenceLength = calculateFenceLength(lengthOfHouse, widthOfFence);
+        double costOfBackyardFence = estimateBackyardFenceCost(fenceLength);
+        double costOfFrontYardFence = estimateFrontYardFenceCost(fenceLength);
         double costOfBathrooms = estimateBathroomCost(lengthOfHouse, widthOfHouse);
-        double costOfFrontYardFence = estimateFrontYardFenceCost(lengthOfHouse, widthOfHouse);
-        double costOfHouse = costOfRoof + costOfFloor + costOfBackyardFence + costOfBathrooms + costOfFrontYardFence;
+
+        double costOfHouse = costOfRoof + costOfFloor + costOfBackyardFence +
+                costOfFrontYardFence + costOfBathrooms;
 
         System.out.println("Roof estimate: $" + costOfRoof);
         System.out.println("Floor estimate: $" + costOfFloor);
         System.out.println("Backyard fence estimate: $" + costOfBackyardFence);
-        System.out.println("Bathroom estimate: $" + costOfBathrooms);
         System.out.println("Front yard fence estimate: $" + costOfFrontYardFence);
+        System.out.println("Bathroom estimate: $" + costOfBathrooms);
         System.out.println("Total house estimate: $" + costOfHouse);
     }
 }
